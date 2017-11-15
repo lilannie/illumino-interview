e9a359c176304f568e19230c09ea505067277736
Annie Steenson <anniesteenso@gmail.com>
2017-11-14 23:18:12 -0600

    Added a timer to the page

--- client/components/Game.js
+++ client/components/Game.js
@@ -1,25 +1,95 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactCountdownClock from 'react-countdown-clock';
+
 import Card from './Card';
 
 export default class Game extends Component {
 	constructor(props) {
 		super(props);
 
+		this.initializeGrid = this.initializeGrid.bind(this);
+		this.handleReset = this.handleReset.bind(this);
+		this.handleOutOfTime = this.handleOutOfTime.bind(this);
 		this.handleClick = this.handleClick.bind(this);
 		this.renderGrid = this.renderGrid.bind(this);
 
 		this.state = {
-			grid: this.props.grid,
-			firstCardClicked: false,
+			grid: this.initializeGrid(),
+			outOfTime: false,
+			startingTime: 10,
 			numMatched: 0,
+			firstCardClicked: false,
 			cardClicked: {
 				id: 0,
 				row: 0,
 				col: 0
 			},
+
 		};
 	}
 
+	initializeGrid() {
+		let grid = [];
+		const {
+			gridLength,
+			numPairs
+		} = this.props;
+
+		// Initialize 2D array
+		for (let i = 0; i < gridLength; i++) {
+			grid.push([]);
+		}
+
+		// Place pairs of pictures to grid randomly
+		for (let i = 1; i <= numPairs; i++) {
+			let randRow = Math.floor(Math.random() * gridLength);
+			let randCol = Math.floor(Math.random() * gridLength);
+			while (grid[randRow][randCol] !== undefined) {
+				randRow = Math.floor(Math.random() * gridLength);
+				randCol = Math.floor(Math.random() * gridLength);
+			}
+
+			grid[randRow][randCol] = {
+				id: i,
+				image: `/images/${i}.jpg`,
+				isTurnedOver: false,
+				disabled: false
+			};
+
+			randRow = Math.floor(Math.random() * gridLength);
+			randCol = Math.floor(Math.random() * gridLength);
+			while (grid[randRow][randCol] !== undefined) {
+				randRow = Math.floor(Math.random() * gridLength);
+				randCol = Math.floor(Math.random() * gridLength);
+			}
+
+			grid[randRow][randCol] = {
+				id: i,
+				image: `/images/${i}.jpg`,
+				isTurnedOver: false,
+				disabled: false
+			};
+		}
+
+		return grid;
+	}
+
+	handleReset() {
+		this.setState({
+			grid: this.initializeGrid(),
+			outOfTime: false,
+			startingTime: 10
+		});
+	}
+
+	handleOutOfTime() {
+		alert('Sorry! You ran out of time. Please, reset the game to play again.');
+
+		this.setState({
+			outOfTime: true
+		});
+	}
 
 	handleClick(id, row, col) {
 		const {
@@ -50,8 +120,8 @@ export default class Game extends Component {
 
 		this.setState({
 			grid,
-			firstCardClicked: !firstCardClicked,
 			numMatched,
+			firstCardClicked: !firstCardClicked,
 			cardClicked: {
 				id,
 				row,
@@ -66,11 +136,15 @@ export default class Game extends Component {
 				<div className="row" key={rowIndex}>
 					{
 						row.map((card, colIndex) => {
-							let handleClick = card.isDisabled ? () => {} : this.handleClick.bind(null, card.id, rowIndex, colIndex);
+							let handleClick =
+								card.isDisabled || this.state.outOfTime
+								? () => {}
+								: this.handleClick.bind(null, card.id, rowIndex, colIndex);
+
 							return (<Card key={`${rowIndex},${colIndex}`}
 							              handleClick={handleClick}
-							              {...card} />
-							);
+							              {...card} />)
+								;
 						})
 					}
 				</div>
@@ -83,10 +157,35 @@ export default class Game extends Component {
 			alert('You won! Yay!');
 		}
 
+		let timer = (<ReactCountdownClock seconds={this.state.startingTime}
+		                                 color="#000"
+		                                 alpha={0.9}
+		                                 size={100}
+		                                 onComplete={this.handleOutOfTime} />);
+		if (this.state.outOfTime) timer = null;
+
 		return (
 			<div className="game">
+					<div className="row justify-content-center">
+							<div className="col">
+								<button type="button" className="btn btn-secondary" onClick={this.handleReset}>Reset</button>
+							</div>
+							<div className="col">
+								{ timer }
+							</div>
+					</div>
 					{ this.renderGrid() }
 			</div>
 		);
 	}
+};
+
+Game.propTypes = {
+	gridLength: PropTypes.number,
+	numPairs: PropTypes.number
+};
+
+Game.defaultProps = {
+	gridLength: 6,
+	numPairs: 18
 };
